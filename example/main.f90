0bc47124b70b9af39e953fde2c433bb5fdd8b5af
mgoonde <miha.gunde@gmail.com>
2024-11-12 17:32:39 +0000

    update main

--- example/main.f90
+++ example/main.f90
@@ -32,7 +32,7 @@ program main
   end do
 
   ! cutoff radius
-  rcut = 6.0_rp
+  rcut = 3.0_rp
 
   ! launch neighbour list
   ! neigh => t_neighbour( nat, typ, pos, lat, rcut )
@@ -40,53 +40,61 @@ program main
   neigh => t_neighbour( nat, typ, pos, lat, rcut, sort_by="distance" )
 
 
-  ! print list of atom index 3
-  ! idx = 3
-  ! n = neigh% get_list( idx, list )
-  ! write(*,*) n
-  ! write(*,*) list
+  ! arbitrary atom index
+  idx = 3
 
-  ! print vectors of idx=3, sorted by distance
-  ! n = neigh% get_veclist( idx, veclist )
-  ! ! it does not include the idx itself, include it manually
-  ! write(*,*) n+1
-  ! write(*,*)
-  ! ! vector of idx is assumed at zero
-  ! write(*,*) typ(idx), [0.0_rp, 0.0_rp, 0.0_rp]
-  ! do i = 1, n
-  !    write(*,*) typ(list(i)),veclist(:,i)
-  ! end do
-  ! deallocate( list, veclist )
+  ! get number of first neighbors
+  n = neigh% get_nn(idx)
+  write(*,*) "number of first neighbors:", n
+
+  ! get list of nearest neighbors
+  write(*,*) "get list"
+  n = neigh% get_nn( idx, list=list )
+  write(*,*) n
+  write(*,"(10i8)") list
+
+  deallocate( list )
 
 
-  ! ! output the neigbor list of all atoms
-  ! do i = 1, nat
-  !    idx = i
-  !    ! n = neigh% get_list( idx, list )
-  !    n = neigh% get_list( idx, list )
-  !    write(*,"(i8)") idx
-  !    ! write(*,"(a,i0)") "idx=",idx
-  !    write(*,"(10i8)") list
-  !    deallocate(list)
-  ! end do
+  ! get list of vectors expanded by `nbond=2`
+  write(*,*) "get list and veclist with nbond=2"
+  n = neigh% get( idx, list=list, veclist=veclist, nbond=2 )
 
-  ! output full struc of each
-  do i = 1, nat
-     n = neigh% get_list( i, list )
-     n = neigh% get_veclist( i, veclist )
-     write(*,*) n+1
-     write(*,*) "from i:",i
-     write(*,"(i3,3f9.4)") typ(i), [0.0_rp, 0.0_rp, 0.0_rp ]
-     do j = 1, n
-        ! write(*,"(i3,3f9.4,2x,i8,f9.5)") typ(list(j)), veclist(:,j), list(j), norm2( veclist(:,j))
-        write(*,"(i3,3f9.4)") typ(list(j)), veclist(:,j)
-     end do
-     deallocate( list, veclist )
+  ! With `nbond>1` we lose the sorting order of neiglist...
+  ! The list does not include the original idx itself, include it manually if needed
+  write(*,*) n+1
+  write(*,*)
+  ! vector of idx is assumed at zero
+  write(*,*) typ(idx), [0.0_rp, 0.0_rp, 0.0_rp], idx
+  do i = 1, n
+     write(*,*) typ(list(i)),veclist(:,i), list(i)
+  end do
+  deallocate( list, veclist )
+
+
+  write(*,*) repeat("-",20)
+
+  ! create an arbitrary list with 4 indices
+  allocate(list(1:4))
+  list = [1, 34, 897, 2 ]
+  ! and their vectors from `pos`
+  allocate(veclist(1:3,1:4))
+  veclist = pos(:,list)
+
+  ! expand this list by 1 bond shell
+  ! The result contains also the original list.
+  n = neigh% expand( 1, list, veclist=veclist )
+  write(*,*) "expanded list:"
+  write(*,"(10i8)") list
+  write(*,*) n
+  write(*,*)
+  do i = 1, n
+     write(*,*) typ(list(i)), veclist(:,i), list(i)
   end do
 
+  deallocate(list, veclist )
+
   deallocate( typ, pos )
-  ! deallocate( list )
-  ! deallocate( veclist )
   deallocate( neigh )
 
 end program
